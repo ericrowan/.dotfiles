5c6b145b983e0282aec10791739c6e62d879dd11
Eric Rowan <erow80@gmail.com>
2015-12-10 08:52:24 -0500

    added a system update file

---
+++ update-system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# update and clean up homebrew packages
+echo 'Updating and cleaning up Homebrew ...'
+brew update ;
+brew upgrade --all
+brew prune ;
+brew doctor ;
+
+# update and cleanup caskroom packages
+echo 'Updating and cleaning up Caskroom packages ...'
+brew cask cleanup ;
+brew cask update ;
+brew cask doctor ;
+
+# update npm packages
+echo 'Updating npm ...'
+npm update -g
+
+# update gems
+echo 'Updating gems ...'
+gem update
+
+# finished
+osascript -e 'display notification "Local dev environment cleaned and/or updated." with title "System Updated"'
