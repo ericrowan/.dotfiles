140ed12053dc0bee5d86b92bab7256d77f98f077
Eric Rowan <erow80@gmail.com>
2016-05-12 08:07:46 -0400

    Update update-system.sh

--- update-system.sh
+++ update-system.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 # update and clean up homebrew packages
-echo 'Updating and cleaning up Homebrew ...'
+echo '------------------------------------'
+echo 'Updating and cleaning up Homebrew...'
+echo '------------------------------------'
 brew update
 brew upgrade --all
 brew prune
 brew doctor
 
 # update and cleanup caskroom packages
-echo 'Updating and cleaning up Caskroom packages ...'
+echo '---------------------------------------------'
+echo 'Updating and cleaning up Caskroom packages...'
+echo '---------------------------------------------'
 brew cask cleanup
 brew cask update
 brew cask doctor
 
 # update npm packages
-echo 'Updating npm ...'
+echo '---------------'
+echo 'Updating npm...'
+echo '---------------'
 npm update -g
 
 # update gems
-echo 'Updating gems ...'
+echo '----------------'
+echo 'Updating gems...'
+echo '----------------'
 gem update --system
 
 # update Homestead
 cd ~/projects/Homestead ; git pull
 
 # finished
-osascript -e 'display notification "Local dev environment cleaned and/or updated." with title "System Updated"'
+echo '----------------------'
+echo '*** SYSTEM UPDATED ***'
+echo '----------------------'
